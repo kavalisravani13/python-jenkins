c54110e0ff8a70118cc2e312b6da4e0b990daca6
kavalisravani13 <kavalisravani13@example.com>
2026-06-29 19:04:04 +0530

    initial python msg

--- calculator.py
+++ calculator.py
@@ -1,7 +1,13 @@
-a = 20
-b = 10
+# calculator.py
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
 
 print("Addition =", a + b)
 print("Subtraction =", a - b)
 print("Multiplication =", a * b)
-print("Division =", a / b)
+
+if b != 0:
+    print("Division =", a / b)
+else:
+    print("Division not possible (cannot divide by zero)")
